6494785bf6d34a3f000cf1769312054a0fa45e45
Owen Robison <orobison@proton.me>
2025-01-28 11:53:33 -0500

    create function to determine winner of one hand

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var humanScore = 0;
+var computerScore = 0;
+
 function getComputerChoice () {
     //returns rock, paper, or scissors, randomly
     var i = parseInt((Math.random() * 100) % 3);
@@ -13,6 +16,68 @@ function getComputerChoice () {
 
 function getHumanChoice () {
     var choice = prompt("Rock, paper, or scissors?");
-    //here's where the data should be cleaned
+    choice = choice.toLowerCase();
     return choice;
+}
+
+function playRound (humanChoice, computerChoice) {
+    switch (humanChoice) {
+        case "rock":
+            switch (computerChoice) {
+                case "rock":
+                    tie(humanChoice, computerChoice);
+                    break;
+                case "paper":
+                    computerWins(humanChoice, computerChoice);
+                    break;
+                case "scissors":
+                    humanWins(humanChoice, computerChoice);
+                    break;
+            }
+            break;
+        case "paper":
+            switch (computerChoice) {
+                case "rock":
+                    humanWins(humanChoice, computerChoice);
+                    break;
+                case "paper":
+                    tie(humanChoice, computerChoice);
+                    break;
+                case "scissors":
+                    computerWins(humanChoice, computerChoice);
+                    break;
+            }
+            break;
+        case "scissors":
+            switch (computerChoice) {
+                case "rock":
+                    computerWins(humanChoice, computerChoice);
+                    break;
+                case "paper":
+                    humanWins(humanChoice, computerChoice);
+                    break;
+                case "scissors":
+                    tie(humanChoice, computerChoice);
+                    break;
+            }
+            break;
+    }
+    return;
+}
+
+function tie (humanChoice, computerChoice) {
+    console.log(`It's a tie! ${humanChoice} and ${computerChoice} are the same.`);
+    return;
+}
+
+function computerWins (humanChoice, computerChoice) {
+    console.log(`You lose :( ${computerChoice} beats ${humanChoice}`);
+    computerScore++;
+    return;
+}
+
+function humanWins (humanChoice, computerChoice) {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    humanScore++;
+    return;
 }
